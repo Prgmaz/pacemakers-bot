2a05191e559587eaaaf5e889290cc3485a02e84b
Programmer101N <namanbaranwal2002@gmail.com>
2021-05-26 13:34:41 +0530

    Fixed bug

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,11 @@
 const { Message } = require("discord.js");
 
-export default new Command(
-	"ping",
-	"Gets the ping of the bot.",
-	(message, args) => {
+module.exports = {
+	name: "ping",
+	description: "Gets the ping of the bot.",
+	execute: (message, args) => {
 		const msg = new Message();
 		msg.content = "Pong!";
 		message.channel.send(msg);
 	}
-);
+};
